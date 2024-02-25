fbe83f1a4a3b4f8db432ec0649a091b95df06470
Vinicius de Andrade <viniciusdeandrade04@gmail.com>
2024-02-25 01:38:14 -0300

    Adding rust formater

--- configs/conform.lua
+++ configs/conform.lua
@@ -1,20 +1,22 @@
 --type conform.options
 local options = {
-	lsp_fallback = true,
+  lsp_fallback = true,
 
-	formatters_by_ft = {
-		lua = { "stylua" },
+  formatters_by_ft = {
+    lua = { "stylua" },
 
-		javascript = { "prettier" },
-		css = { "prettier" },
-		html = { "prettier" },
+    javascript = { "prettier" },
 
-	},
+    typescript = { "prettier", "eslint_d" },
+    css = { "prettier" },
+    html = { "prettier" },
+
+    rust = { "rustfmt" },
+  },
   format_on_save = {
     timeout_ms = 500,
     lsp_fallback = true,
   },
-
 }
 
 require("conform").setup(options)
